5e5ed05b69943fbbf1f044d6f324398cf545b3cc
MatiasMartinez-22 <matias.e.martinez1993@gmail.com>
2026-08-20 15:29:22 -0300

    persona.cs
    agregando using y namespace

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -1,4 +1,4 @@
-public class Persona {
+/*public class Persona {
     [Key]
     [Display(Name = "Código")]
     public int id { get; set; }
@@ -13,4 +13,33 @@ public class Persona {
     public string Telefono { get; set; } = string.Empty;
     [Required, EmailAddress]
     public string Correo { get; set; } = string.Empty;
+}*/
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Inmobiliaria_.Net_Core.Models
+{
+    public class Persona
+    {
+        [Key]
+        [Display(Name = "Código")]
+        public int id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        public string Apellido { get; set; } = string.Empty;
+
+        [Required]
+        public string DNI { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Teléfono")]
+        public string Telefono { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; } = string.Empty;
+    }
 }
