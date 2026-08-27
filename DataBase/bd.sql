f6936f96b50b05b74b5f67db541ef6e5b0641c89
Alberto Maximiliano Daroni Escobar <163307152+AlbertDaroni@users.noreply.github.com>
2026-08-27 20:42:32 -0300

    Corrección del Script de la Base de Datos

--- DataBase/bd.sql
+++ DataBase/bd.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE Inmobiliaria;
-
 USE Inmobiliaria;
 
 CREATE TABLE Propietarios (
@@ -20,6 +19,56 @@ CREATE TABLE Inquilinos (
     Correo VARCHAR(100) NOT NULL UNIQUE
 );
 
+CREATE TABLE Tipo_Inmuebles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(100) NOT NULL,
+    Apellido VARCHAR(100) NOT NULL,
+    Correo VARCHAR(100) NOT NULL UNIQUE,
+    Contraseña VARCHAR(255) NOT NULL,
+    Avatar VARCHAR(255) NOT NULL,
+    Rol VARCHAR(50) NOT NULL,
+    Estado VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE Inmuebles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    direccion VARCHAR(50) NOT NULL,
+    cupo INT NOT NULL,
+    latitud DECIMAL(10,8) NOT NULL,
+    longitud DECIMAL(11,8) NOT NULL,
+    precio_dia DECIMAL(10,2) NOT NULL,
+    porcentaje_seña DECIMAL(10,2) NOT NULL,
+    estado VARCHAR(1) NOT NULL,
+    id_propietario INT,
+    id_tipo INT,
+
+    CONSTRAINT fk_inmueble_propietario
+    FOREIGN KEY (id_propietario)
+    REFERENCES Propietarios(id),
+
+    CONSTRAINT fk_inmueble_tipo
+    FOREIGN KEY (id_tipo)
+    REFERENCES Tipo_Inmuebles(id)
+);
+
+CREATE TABLE Imagen_Inmuebles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    url VARCHAR(255) NOT NULL,
+    esPortada BOOLEAN NOT NULL,
+    orden INT NOT NULL,
+    id_inmueble INT,
+
+    CONSTRAINT fk_imagen_inmuebles_inmueble
+    FOREIGN KEY (id_inmueble)
+    REFERENCES Inmuebles(id)
+);
+
 CREATE TABLE Reservas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     fecha_creacion DATE NOT NULL,
@@ -34,72 +83,22 @@ CREATE TABLE Reservas (
     id_usuario_creador INT,
     id_usuario_finalizador INT,
 
-    CONSTRAINT fk_reserva_inquilinos,
-    FOREIGN KEY (id_inquilino),
+    CONSTRAINT fk_reserva_inquilinos
+    FOREIGN KEY (id_inquilino)
     REFERENCES Inquilinos(id),
 
-    CONSTRAINT fk_reserva_inmuebles,
-    FOREIGN KEY (id_inmueble),
-    REFERENCES Inmuebles(id),
-    ON UPDATE CASCADE, ON DELETE CASCADE,
+    CONSTRAINT fk_reserva_inmuebles
+    FOREIGN KEY (id_inmueble)
+    REFERENCES Inmuebles(id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
-    CONSTRAINT fk_reserva_usuarios_creadores,
-    FOREIGN KEY (id_usuario_creador),
+    CONSTRAINT fk_reserva_usuarios_creadores
+    FOREIGN KEY (id_usuario_creador)
     REFERENCES Usuarios(id),
 
-    CONSTRAINT fk_reserva_usuarios_finalizadores,
-    FOREIGN KEY (id_usuario_finalizador),
+    CONSTRAINT fk_reserva_usuarios_finalizadores
+    FOREIGN KEY (id_usuario_finalizador)
     REFERENCES Usuarios(id)
-);
-
-CREATE TABLE Inmuebles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    direccion VARCHAR(50) NOT NULL,
-    cupo INT NOT NULL,
-    latitud DECIMAL(10,2) NOT NULL,
-    longitud DECIMAL(10,2) NOT NULL,
-    precio_dia DECIMAL(10,2) NOT NULL,
-    porcentaje_seña DECIMAL(10,2) NOT NULL,
-    estado VARCHAR(1) NOT NULL,
-    id_propietario INT,
-    id_tipo INT,
-
-    CONSTRAINT fk_inmueble_propietario,
-    FOREIGN KEY (id_propietario),
-    REFERENCES Propietarios(id),
-
-    CONSTRAINT fk_inmueble_tipo
-    FOREIGN KEY (id_tipo),
-    REFERENCES Tipo_Inmuebles(id)
-);
-
-CREATE TABLE Tipo_Inmuebles (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50) NOT NULL,
-    descripcion VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE Imagen_Inmuebles (
-    id PRIMARY KEY AUTO_INCREMENT,
-    url VARCHAR NOT NULL,
-    esPortada BOOLEAN NOT NULL,
-    orden INT NOT NULL,
-    id_inmueble INT,
-
-    CONSTRAINT fk_imagen_inmuebles_inmueble,
-    FOREIGN KEY (id_inmueble),
-    REFERENCES Inmuebles(id)
-);
-
-CREATE TABLE Usuarios (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    Nombre VARCHAR(100) NOT NULL,
-    Apellido VARCHAR(100) NOT NULL,
-    Correo VARCHAR(100) NOT NULL UNIQUE
-    Contraseña VARCHAR(50) NOT NULL,
-    Avatar VARCHAR(50) NOT NULL,
-    Rol VARCHAR(50) NOT NULL,
-    Estado VARCHAR(1) NOT NULL
 );
 
 CREATE TABLE Pagos (
@@ -113,15 +112,15 @@ CREATE TABLE Pagos (
     id_usuario_creador INT,
     id_usuario_finalizador INT,
 
-    CONSTRAINT fk_pago_reserva,
-    FOREIGN KEY (id_reserva),
+    CONSTRAINT fk_pago_reserva
+    FOREIGN KEY (id_reserva)
     REFERENCES Reservas(id),
 
-    CONSTRAINT fk_pago_usuario_creador,
-    FOREIGN KEY (id_usuario_creador),
+    CONSTRAINT fk_pago_usuario_creador
+    FOREIGN KEY (id_usuario_creador)
     REFERENCES Usuarios(id),
 
-    CONSTRAINT fk_pago_usuario_finalizador,
-    FOREIGN KEY (id_usuario_finalizador),
-    REFERENCES Usuarios(id),
+    CONSTRAINT fk_pago_usuario_finalizador
+    FOREIGN KEY (id_usuario_finalizador)
+    REFERENCES Usuarios(id)
 );
