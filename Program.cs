650a0402b015ca80233645b340ce4d0bc4bac36d
Alberto Maximiliano Daroni Escobar <163307152+AlbertDaroni@users.noreply.github.com>
2026-09-03 17:04:35 -0300

    Rutas hacia las nuevas vistas

--- Program.cs
+++ Program.cs
@@ -7,31 +7,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // Inyección de dependencia del repositorio de Propietarios
-builder.Services.AddScoped<
-    IRepositorioPropietario,
-    RepositorioPropietarioMySql
->();
-
-builder.Services.AddScoped<
-    IRepositorio_Inquilino,
-    RepositorioInquilinoMySql
->();
+builder.Services.AddScoped<IRepositorioPropietario, RepositorioPropietarioMySql>();
+builder.Services.AddScoped<IRepositorio_Inquilino, RepositorioInquilinoMySql>();
+// ¡¡¡¡¡¡¡¡¡ IMPORTATE !!!!!!!!!
+// Corregir los nombres de los 4 AddScoped<...> ("Repositorio<nombre>MySql.cs")
+builder.Services.AddScoped<IRepositorio_Imagen_Inmueble, RepositorioImagenInmuebleMySql>();
+builder.Services.AddScoped<IRepositorio_Inmueble, RepositorioInmuebleMySql>();
+builder.Services.AddScoped<IRepositorio_Reserva, RepositorioReservaMySql>();
+builder.Services.AddScoped<IRepositorio_Tipo_Inmueble, RepositorioTipo_InmuebleMySql>();
 
 var app = builder.Build();
 
 // Configuración del pipeline HTTP
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
 
 // Ruta MVC por defecto
